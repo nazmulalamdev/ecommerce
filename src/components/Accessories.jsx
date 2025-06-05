f8e88573df49aab6e3b9396a11709960c97795ab
Nazmul Alam <nazmulcse08@gmail.com>
2025-06-05 16:47:11 +0600

    upadate

---
+++ src/components/Accessories.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Container from "./commonLayout/Container";
+
+const Accessories = () => {
+  return (
+    <>
+      <Container>
+        <div className="mt-[105px]">
+          <h3 className="text-center font-['Prompt'] font-extrabold text-[74px]">
+            What <span className="text-[#5E1EE5]">we</span> provide?
+          </h3>
+          <div className="flex items-center justify-between mt-[51px]">
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[85px] h-[85px] rounded-full shadow-[0px_4px_24px_-1px_rgba(0,0,0,0.08)] flex items-center justify-center">
+                <img src="images/camera.png" alt="accessories" />
+              </div>
+
+              <p className="max-w-[118px] text-center mt-6 font-['Montserrat'] font-medium text-lg text-[#2C2C2C]">
+                Computer & Laptop
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Accessories;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Faq from "../components/Faq";
+import Accessories from "../components/Accessories";
 
 const HomePage = () => {
   return (
     <>
       <Banner />
+      <Accessories />
       <Faq />
     </>
   );
